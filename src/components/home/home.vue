e86a21811cf64c1c0e17072215e96205a63f806b
siyuzheng <1163834908@qq.com>
2019-12-03 15:38:55 +0800

    容器布局

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    home
-  </div>
+  <el-container class="contain">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
-<style lang="" scoped>
+<style lang="">
+.contain {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
 </style>
