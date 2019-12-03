f7c4376d292f873fd1f2f5e11d4e4ab38765a52a
siyuzheng <1163834908@qq.com>
2019-12-03 21:01:55 +0800

    引入侧边栏

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -3,8 +3,8 @@
     <el-header class="header">
       <el-row>
         <el-col :span="4" class="logo">
-          <div class="grid-content bg-purple" >
-            <img src="../../assets/logo.png" alt="无法显示图片"/>
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="无法显示图片" width="200" />
           </div>
         </el-col>
         <el-col :span="18" class="middle">
@@ -20,7 +20,77 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-aside width="200px" class="aside">
+        <el-menu :unique-opened="true">
+          <!-- 用户管理 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 权限管理 -->
+           <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 商品管理 -->
+           <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">分类参数</span>
+            </el-menu-item>
+             <el-menu-item index="3-3">
+              <i class="el-icon-menu"></i>
+              <span slot="title">商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 订单管理 -->
+           <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 数据统计 -->
+           <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -36,15 +106,17 @@ export default {}
 }
 .header {
   background-color: #b3c0d1;
+  padding-left: 0;
 }
 .aside {
   background-color: #d3dce6;
+  height: 100%;
 }
 .main {
   background-color: #e9eef3;
   height: 100%;
 }
-.logo{
+.logo {
   padding-top: 2px;
 }
 .middle {
