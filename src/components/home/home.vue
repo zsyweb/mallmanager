4673480bc45d55075b057ad904eb1e5a03697901
siyuzheng <1163834908@qq.com>
2019-12-03 16:07:22 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,24 @@
 <template>
   <el-container class="contain">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4" class="logo">
+          <div class="grid-content bg-purple" >
+            <img src="../../assets/logo.png" alt="无法显示图片"/>
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <div class="grid-content bg-purple-light">
+            <h3>电商后台管理系统</h3>
+          </div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+            <a href="#" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -25,5 +43,15 @@ export default {}
 .main {
   background-color: #e9eef3;
   height: 100%;
+}
+.logo{
+  padding-top: 2px;
+}
+.middle {
+  text-align: center;
+}
+.loginout {
+  text-decoration: none;
+  line-height: 60px;
 }
 </style>
